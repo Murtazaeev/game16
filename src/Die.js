1410673b238a7aeccb8cca63c5cc69af4b076150
Temur Murtazaev <temur.murtazaeveurope@gmail.com>
2022-02-09 17:08:48 +0100

    modifying dice die rule and game components

--- src/Die.js
+++ src/Die.js
@@ -2,15 +2,32 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+
+  static defaultProps = {
+    numberWords: ["one", "two", "three", "four", "five", "six"]
+  };
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.props.handleClick(this.props.idx)
+  }
   render() {
+    const { numberWords, locked, val, disabled } = this.props;
+    let classes = `Die fas fa-dice-${numberWords[val - 1]}
+     fa-5x`;
+    if (locked) classes += "Die-locked";
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
+      <i
+        className={classes}
+        onClick={this.handleClick}
+        disabled={disabled}
       >
-        {this.props.val}
-      </button>
+
+      </i>
     );
   }
 }
